0140f2090caad4cb6a5b456373bc060c038294c5
Bhargav Parekh <bhargavparekh@Bhargavs-Mac.ht.home>
2024-03-07 03:16:20 -0500

    added slider and swiper

--- my-app/components/ProjectCard.jsx
+++ my-app/components/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from "next/image"
-import {Card, CardHeader} from "./ui/Card"
+import {Card, CardHeader} from "./ui/card"
 import {Github, Link2Icon} from 'lucide-react'
 import {Badge} from './ui/badge'
 
