23ee2e2179e3f764798c11574a9bbe57c094c0e7
Bhargav Parekh <bhargavparekh@Bhargavs-Mac.ht.home>
2024-03-09 01:42:59 -0500

    finally done :)

--- my-app/components/ProjectCard.jsx
+++ my-app/components/ProjectCard.jsx
@@ -11,7 +11,7 @@ const ProjectCard =({project}) =>{
                 <div className='relative w-full h-[300px] flex items-center justify-center 
                 bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light 
                 xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat overflow-hidden'>
-                    <Image className='absolute bottom-0 shadow-2xl' src={project.image} width ={247} height={250} alt='' priority />
+                    <Image style={{borderRadius: '20px', overflow: 'hidden'}} className='absolute bottom-0 shadow-2xl' src={project.image} width ={247} height={250} alt='' priority />
                 {/*Buttons */}
                 <div className='flex gap-x-4'>
                     <Link href={project.link} className='bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200'>
